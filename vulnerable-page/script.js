be728a79df3378c2db6ca858e1738c83b1503c4f
raghavkhare98 <khare08rma@gmail.com>
2024-07-11 16:38:03 -0400

    Add claim button

---
+++ vulnerable-page/script.js
@@ -0,0 +1,6 @@
+const claimButton = document.getElementById('#claim-button');
+
+claimButton.addEventListener('click', (e) => {
+    console.log("jdsas");
+    alert("Thank you for claiming the coupon! You will receive your exclusive coupon code shortly on your email");
+})
