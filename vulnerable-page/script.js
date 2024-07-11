1945ceeb5337f64f13a65caba8ff034e07c07398
raghavkhare98 <khare08rma@gmail.com>
2024-07-11 16:47:07 -0400

    Add validation for input fields in vulnerable page

--- vulnerable-page/script.js
+++ vulnerable-page/script.js
@@ -1,6 +1,10 @@
-const claimButton = document.getElementById('#claim-button');
-
+const claimButton = document.getElementById('claim-button');
+const emailInput = document.getElementById('email-input');
+const passInput = document.getElementById('password-input');
 claimButton.addEventListener('click', (e) => {
-    console.log("jdsas");
+    if(emailInput.value == "" || passInput.value == ""){
+        alert("Please enter your email and password to claim coupon");
+    } else{
     alert("Thank you for claiming the coupon! You will receive your exclusive coupon code shortly on your email");
+    }
 })
